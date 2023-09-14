31d5beabd1b5eaf17861fa4b75cdbcd1a84e3911
Eda Dural <edaadurall@gmail.com>
2023-09-14 11:04:58 +0300

    kurulum

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <Button type="primary" danger>Primary</Button>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
