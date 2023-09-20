dab89385eada32119b649e314699e3d380ff2b32
Eda Dural <edaadurall@gmail.com>
2023-09-20 20:11:06 +0300

    categories component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      home
-    </div>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-85px)] pb-60">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <div className="">products</div>
+        </div>
+        <div className="">
+          <div className="">cart totals</div>
+        </div>
+      </div>
+    </>
   );
 }
 
