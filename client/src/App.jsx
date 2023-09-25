bb736d14a0b5ebe07d1284dcebaeba50f2caac02
Eda Dural <edaadurall@gmail.com>
2023-09-25 17:42:24 +0300

    cart totals component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import CartTotals from "./components/cart/CartTotals";
 import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
 import Products from "./components/products/Products";
@@ -7,16 +8,14 @@ function App() {
     <>
       <Header />
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-85px)] pb-60">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-85px)] pb-60">
           <Categories />
         </div>
         <div className="products flex-[8]">
-          <div className="">
-            <Products />
-          </div>
+            <Products />       
         </div>
-        <div className="">
-          <div className="">cart totals</div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals />
         </div>
       </div>
     </>
