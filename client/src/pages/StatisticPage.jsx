0693a3711593bfc08a36f430bf347c65bc2b2e1d
Eda Dural <edaadurall@gmail.com>
2023-09-29 19:17:28 +0300

    statistic card component

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import Header from '../components/header/Header'
+import StatisticCard from '../components/statistics/StatisticCard'
 
 const StatisticPage = () => {
     return (
         <>
             <Header />
-            <div className="px-6">
+            <div className="px-5">
                 <h1 className="text-3xl font-bold text-center mb-3">İstatistiklerim</h1>
                 <div>
                     <h2 className='text-lg'>Hoş geldin <span className='text-green-700 font-bold'>admin</span>.</h2>
-                    <div className="statistic-cards grid grid-cols-4 my-5">
-                        <div className="card-item bg-gray-800 p-8 rounded-lg">
-                            <div className='flex gap-x-4'>
-                                <div className='rounded-full bg-white w-16 h-16 p-3'>
-                                    <img src='images/user.png' alt='' />
-                                </div>
-                                <div className='text-white'>
-                                    <p className='mb-2 text-lg font-medium text-gray-400'>Toplam Müşteri</p>
-                                    <p className='text-xl font-bold text-gray-200'>6</p>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-2 md:gap-6 gap-3">
+                        <StatisticCard
+                            title={"Toplam Müşteri"}
+                            amount={"6"}
+                            img="images/user.png"
+                        />
+                        <StatisticCard
+                            title={"Toplam Kazanç"}
+                            amount={"660.96₺"}
+                            img="images/money.png"
+                        />
+                        <StatisticCard
+                            title={"Toplam Satış"}
+                            amount={"6"}
+                            img="images/sale.png"
+                        />
+                        <StatisticCard
+                            title={"Toplam Ürün"}
+                            amount={"28"}
+                            img="images/product.png"
+                        />
                     </div>
                 </div>
             </div>
