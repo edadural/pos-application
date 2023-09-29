5a8c3530d473db14a386bec0896099507c0a26cc
Eda Dural <edaadurall@gmail.com>
2023-09-29 18:43:19 +0300

    statistic card

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -8,7 +8,20 @@ const StatisticPage = () => {
             <div className="px-6">
                 <h1 className="text-3xl font-bold text-center mb-3">İstatistiklerim</h1>
                 <div>
-                    <h2 className='text-lg'>Hoş geldin <span className='text-green-700 font-bold text-xl'>admin</span>.</h2>
+                    <h2 className='text-lg'>Hoş geldin <span className='text-green-700 font-bold'>admin</span>.</h2>
+                    <div className="statistic-cards grid grid-cols-4 my-5">
+                        <div className="card-item bg-gray-800 p-8 rounded-lg">
+                            <div className='flex gap-x-4'>
+                                <div className='rounded-full bg-white w-16 h-16 p-3'>
+                                    <img src='images/user.png' alt='' />
+                                </div>
+                                <div className='text-white'>
+                                    <p className='mb-2 text-lg font-medium text-gray-400'>Toplam Müşteri</p>
+                                    <p className='text-xl font-bold text-gray-200'>6</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
