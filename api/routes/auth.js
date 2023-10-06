2a7ffe8f2d27383a2013396756a33f36150db977
Eda Dural <edaadurall@gmail.com>
2023-10-06 14:32:46 +0300

    user login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -9,12 +9,33 @@ router.post("/register", async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new User({
-            username, 
+            username,
             email,
             password: hashedPassword,
         });
         await newUser.save();
         res.status(200).json("A new user created successfully.");
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+// login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).send({error: "User not found!"});        // eslesen user yoksa error
+
+        const validPassword = await bcrypt.compare(   // iki sifreyi karsilastirma
+            req.body.password,
+            user.password
+        );
+
+        if (!validPassword){
+            res.status(403).json("Invalid password!");
+        }else {
+            res.status(200).json(user);
+        }
     } catch (error) {
         res.status(400).json(error);
     }
