fc8110090bc8dd419071176982a44e2881c01caa
Eda Dural <edaadurall@gmail.com>
2023-10-06 13:59:43 +0300

    güvenli parola

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,18 @@
 const User = require("../models/User.js");
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 // register
 router.post("/register", async (req, res) => {
     try {
-        const newUser = new User(req.body);
+        const { username, email, password } = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({
+            username, 
+            email,
+            password: hashedPassword,
+        });
         await newUser.save();
         res.status(200).json("A new user created successfully.");
     } catch (error) {
