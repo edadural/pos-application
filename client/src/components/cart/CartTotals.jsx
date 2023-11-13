0ea9ad24fa83116236f8d440e0c05d0d7237e872
Eda Dural <edaadurall@gmail.com>
2023-11-13 16:39:48 +0300

    ürün miktarı + -

--- client/src/components/cart/CartTotals.jsx
+++ client/src/components/cart/CartTotals.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { ClearOutlined, PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { deleteCart } from '../../redux/cartSlice';
+import { deleteCart, increase, decrease } from '../../redux/cartSlice';
 
 const CartTotals = () => {
     const cart = useSelector((state) => state.cart);
@@ -13,37 +13,49 @@ const CartTotals = () => {
         <div className='cart h-full max-h-[calc(100vh_-_90px)] flex flex-col'>
             <h2 className='bg-blue-600 text-center py-4 text-white font-bold tracking-wide'>Sepetteki Ürünler</h2>
             <ul className="cart-items px-2 flex flex-col gap-y-3 pt-2 py-2 md:overflow-y-auto">
-                {cart.cartItems.map((item) => (  // cI içinde kaç eleman varsa her birini dön, her döndüğünde bir item gönder 
-                    <li className="cart-item flex justify-between" key={item._id}>
-                        <div className='flex items-center'>
-                            <img
-                                src={item.img}
-                                alt=''
-                                className='w-14 h-14 object-cover cursor-pointer'
-                                onClick={() => dispatch(deleteCart(item))}
-                            />
-                            <div className='flex flex-col ml-2'>
-                                <b>{item.title}</b>
-                                <span>{item.price}₺ x {item.quantity}</span>
+                {cart.cartItems.length > 0
+                    ? cart.cartItems.map((item) => (  // cI içinde kaç eleman varsa her birini dön, her döndüğünde bir item gönder 
+                        <li className="cart-item flex justify-between" key={item._id}>
+                            <div className='flex items-center'>
+                                <img
+                                    src={item.img}
+                                    alt=''
+                                    className='w-14 h-14 object-cover cursor-pointer'
+                                    onClick={() => dispatch(deleteCart(item))}
+                                />
+                                <div className='flex flex-col ml-2'>
+                                    <b>{item.title}</b>
+                                    <span>{item.price}₺ x {item.quantity}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className='flex items-center gap-x-1'>
-                            <Button
-                                type='primary'
-                                size='small'
-                                className='w-full !rounded-full'
-                                icon={<PlusCircleOutlined />}
-                            />
-                            <span className='font-bold'>{item.quantity}</span>
-                            <Button
-                                type='primary'
-                                size='small'
-                                className='w-full !rounded-full'
-                                icon={<MinusCircleOutlined />}
-                            />
-                        </div>
-                    </li>
-                ))}
+                            <div className='flex items-center'>
+                                <Button
+                                    type='primary'
+                                    size='small'
+                                    className='w-full !rounded-full'
+                                    icon={<PlusCircleOutlined />}
+                                    onClick={() => dispatch(increase(item))}
+                                />
+                                <span className='font-bold w-6 inline-block text-center'>{item.quantity}</span>
+                                <Button
+                                    type='primary'
+                                    size='small'
+                                    className='w-full !rounded-full'
+                                    icon={<MinusCircleOutlined />}
+                                    onClick={() => {
+                                        if (item.quantity === 1) {
+                                            if (window.confirm("Ürün silinsin mi?")) {
+                                                dispatch(decrease(item));
+                                            }
+                                        }
+                                        if (item.quantity > 1) {
+                                            dispatch(decrease(item));
+                                        }
+                                    }}
+                                />
+                            </div>
+                        </li>
+                    )) : "Sepette hiç ürün yok..."}
             </ul>
             <div className="cart-totals mt-auto">
                 <div className='border-t border-b'>
@@ -51,7 +63,7 @@ const CartTotals = () => {
                         <b>Ara toplam</b>
                         <span>
                             {cart.total > 0
-                                ? cart.total.toFixed(2) 
+                                ? cart.total.toFixed(2)
                                 : 0}        {/* virgülden sonra 2 basamak */}
                             ₺
                         </span>
--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -14,22 +14,39 @@ const cartSlice = createSlice({
             );
 
             if (findCartItem) {
-                findCartItem.quantity += 1;
+                findCartItem.quantity++;
             } else {
                 state.cartItems.push(action.payload);
             }
-
             state.total += action.payload.price;
         },
         deleteCart: (state, action) => {
             state.cartItems = state.cartItems.filter(
                 (item) => item._id !== action.payload._id
             );
-
             state.total -= action.payload.price * action.payload.quantity;
+        },
+        increase: (state, action) => {
+            const cartItem = state.cartItems.find(
+                (item) => item._id === action.payload._id
+            );
+            cartItem.quantity++;
+            state.total += cartItem.price;
+        },
+        decrease: (state, action) => {
+            const cartItem = state.cartItems.find(
+                (item) => item._id === action.payload._id
+            );
+            cartItem.quantity--;
+            if (cartItem.quantity === 0) {
+                state.cartItems = state.cartItems.filter(
+                    (item) => item._id !== action.payload._id
+                );
+            }
+            state.total -= cartItem.price;
         },
     },
 });
 
-export const { addProduct, deleteCart } = cartSlice.actions;   // reducers içerisinde yazılanlar actions diye geçiyo ---- bi yerde çağırmak istiyorsak export olarak yazılmalı
+export const { addProduct, deleteCart, increase, decrease } = cartSlice.actions;   // reducers içerisinde yazılanlar actions diye geçiyo ---- bi yerde çağırmak istiyorsak export olarak yazılmalı
 export default cartSlice.reducer;
