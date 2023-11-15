3f9a2886ae953062fee9925583c07001cf8647b8
Eda Dural <edaadurall@gmail.com>
2023-11-15 10:26:03 +0300

    müşteri sayfası

--- client/src/pages/CustomerPage.jsx
+++ client/src/pages/CustomerPage.jsx
@@ -1,37 +1,41 @@
 import { Table } from "antd";
 import Header from "../components/header/Header";
+import { useEffect, useState } from "react";
 
 const CustomerPage = () => {
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
+    const [billItems, setBillItems] = useState([]);
+
+    useEffect(() => {
+        const getBills = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/bills/get-all");
+                const data = await res.json();
+                setBillItems(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getBills();
+    }, []);
 
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
+            title: 'Müşteri Adı',
+            dataIndex: 'customerName',
+            key: 'customerName',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Telefon Numarası',
+            dataIndex: 'customerPhoneNumber',
+            key: 'customerPhoneNumber',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
+            title: 'İşlem Tarihi',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            render: (text) => {
+                return <span>{text?.substring(0, 10)}</span>
+            }
         },
     ];
 
@@ -41,7 +45,7 @@ const CustomerPage = () => {
             <div className="px-6">
                 <h1 className="text-3xl font-bold text-center mb-3">Müşteriler</h1>
                 <Table
-                    dataSource={dataSource}
+                    dataSource={billItems}
                     columns={columns}
                     bordered
                     agination={false}
