81cefc440dcd4be1b3729c2aeb16b1c4216aaaf0
Eda Dural <edaadurall@gmail.com>
2023-11-16 11:58:10 +0300

    cart local storage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,17 @@ import StatisticPage from "./pages/StatisticPage";
 import ProductPage from "./pages/ProductPage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart])
+
+
   return (
     <BrowserRouter>
       <Routes>
