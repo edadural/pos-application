965dbf14b65574e6ae086664c877a9b279912310
Eda Dural <edaadurall@gmail.com>
2023-11-16 10:32:48 +0300

    bill page filter

--- client/src/pages/BillPage.jsx
+++ client/src/pages/BillPage.jsx
@@ -1,12 +1,131 @@
-import { Button, Table } from "antd";
+import { Button, Input, Space, Table } from "antd";
 import Header from "../components/header/Header";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import PrintBill from "../components/bills/PrintBill";
+import Highlighter from "react-highlight-words";
+import { SearchOutlined } from "@ant-design/icons";
 
 const BillPage = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [billItems, setBillItems] = useState([]);
     const [customer, setCustomer] = useState();
+    const [searchText, setSearchText] = useState("");
+    const [searchedColumn, setSearchedColumn] = useState("");
+    const searchInput = useRef(null);
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText("");
+    };
+
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({
+            setSelectedKeys,
+            selectedKeys,
+            confirm,
+            clearFilters,
+            close,
+        }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) =>
+                        setSelectedKeys(e.target.value ? [e.target.value] : [])
+                    }
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: "block",
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? "#1890ff" : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: "#ffc069",
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ""}
+                />
+            ) : (
+                text
+            ),
+    });
 
     useEffect(() => {
         const getBills = async () => {
@@ -26,11 +145,13 @@ const BillPage = () => {
             title: 'Müşteri Adı',
             dataIndex: 'customerName',
             key: 'customerName',
+            ...getColumnSearchProps("customerName"),
         },
         {
             title: 'Telefon Numarası',
             dataIndex: 'customerPhoneNumber',
             key: 'customerPhoneNumber',
+            ...getColumnSearchProps("customerPhoneNumber"),
         },
         {
             title: 'Oluşturma Tarihi',
@@ -44,6 +165,7 @@ const BillPage = () => {
             title: 'Ödeme Yöntemi',
             dataIndex: 'paymentMode',
             key: 'paymentMode',
+            ...getColumnSearchProps("paymentMode"),
         },
         {
             title: 'Toplam Fiyat',
@@ -51,7 +173,8 @@ const BillPage = () => {
             key: 'totalAmount',
             render: (text) => {
                 return <span>{text}₺</span>
-            }
+            },
+            sorter: (a, b) => a.totalAmount - b.totalAmount,
         },
         {
             title: 'Actions',
@@ -86,7 +209,7 @@ const BillPage = () => {
                     pagination={false}
                     scroll={{
                         x: 1000,
-                        y:300,
+                        y: 300,
                     }}
                 />
             </div>
